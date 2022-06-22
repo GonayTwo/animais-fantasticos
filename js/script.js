8bb18e652e5cc32287146eec4b1c72d77ecb1405
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-06-22 16:42:51 -0300

    Refatoração do fetch BitCoin

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropDownMenu from "./modules/dropdownmenu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"');
@@ -29,5 +29,5 @@ initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
 fetchAnimais('../animaisapi.json', '.numeros-grid');
-initFetchBitcoin();
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 initAnimacaoScroll();
